e9949a5b3e2d1e5897ee1457c815e20172551419
mlamamra <mlamamra@dawan.fr>
2024-06-11 12:52:52 +0200

    remplacer le docker cp avec un volume bind mounts: point de montage

--- app-php/app-php.sh
+++ app-php/app-php.sh
@@ -25,21 +25,23 @@ docker run \
        --name app-php-fpm \
        -d --restart unless-stopped \
        --network app-php \
+       -v /vagrant/app-php/index.php:/srv/index.php \
        bitnami/php-fpm:8.3-debian-12
 
-docker cp /vagrant/app-php/index.php app-php-fpm:/srv/index.php
+# docker cp /vagrant/app-php/index.php app-php-fpm:/srv/index.php
 
 docker run \
        --name app-php-nginx \
        -d --restart unless-stopped \
        -p 8081:80 \
        --network app-php \
+       -v /vagrant/app-php/php8.3.conf:/etc/nginx/conf.d/php8.3.conf \
        nginx:1.27-bookworm
 
-docker cp /vagrant/app-php/php8.3.conf \
-       app-php-nginx:/etc/nginx/conf.d/php8.3.conf
+# docker cp /vagrant/app-php/php8.3.conf \
+#        app-php-nginx:/etc/nginx/conf.d/php8.3.conf
 
 # changement de config  => redémarrer le service nginx
 # normalement avec systemd => mais avec docker on pas besoin
 # de systemd puisqu'on a --restart
-docker restart app-php-nginx
+# docker restart app-php-nginx
