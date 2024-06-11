6c785a6f5df62f599965252109c7ae490108a3bb
mlamamra <mlamamra@dawan.fr>
2024-06-11 11:11:50 +0200

    config nginx <-> php

--- app-php/app-php.sh
+++ app-php/app-php.sh
@@ -6,14 +6,22 @@
 [[ ! -z $(docker ps -aq --filter "name=app-php-*") ]] && docker rm -f $(docker ps -aq -f "name=app-php-*")
 
 docker run \
+       --name app-php-fpm \
+       -d --restart unless-stopped \
+       bitnami/php-fpm:8.3-debian-12
+
+docker cp /vagrant/app-php/index.php app-php-fpm:/srv/index.php
+
+docker run \
        --name app-php-nginx \
        -d --restart unless-stopped \
        -p 8081:80 \
        nginx:1.27-bookworm
 
-docker run \
-       --name app-php-fpm \
-       -d --restart unless-stopped \
-       bitnami/php-fpm:8.3-debian-12
+docker cp /vagrant/app-php/php8.3.conf \
+       app-php-nginx:/etc/nginx/conf.d/php8.3.conf
 
-
+# changement de config  => redémarrer le service nginx
+# normalement avec systemd => mais avec docker on pas besoin
+# de systemd puisqu'on a --restart
+docker restart app-php-nginx
